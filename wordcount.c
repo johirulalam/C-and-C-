005a844055c6ed1a7b4adfb21a3ab6c3afbe8dba
johirulalam <50805737+johirulalam@users.noreply.github.com>
2020-03-10 00:02:23 +0600

    Update wordcount.c

--- wordcount.c
+++ wordcount.c
@@ -4,9 +4,12 @@ int main() {
     int i,word =0;
     char name[] = "Johirul Alam soikat";
     for(i = 0; name[i]!='\0'; i++) {
+        
+        /* the name variable could be null so we have to declare word=0 not word=1 after checking not null then we call word=1   */
             if(word == 0){
                 word = 1;
             }
+        /* it could be white space that means it counts every  space so we have to check that previous one is not space  */ 
         if(name[i]==32 && name[i-1]!=32){
 
             word++;
